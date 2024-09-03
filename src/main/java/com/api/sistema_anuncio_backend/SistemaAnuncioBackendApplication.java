f3fa71a4e4b8ba26f87d1885fbebee418d0a65c4
Daniel Penelva <d4n.andrade@gmail.com>
2024-09-03 20:13:55 -0300

    feat: criando projeto SpringBoot

---
+++ src/main/java/com/api/sistema_anuncio_backend/SistemaAnuncioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.api.sistema_anuncio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAnuncioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAnuncioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/sistema_anuncio_backend/SistemaAnuncioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.sistema_anuncio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAnuncioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
