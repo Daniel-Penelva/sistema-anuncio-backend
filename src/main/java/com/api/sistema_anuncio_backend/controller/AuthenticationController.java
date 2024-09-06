48c6cafbdf3f4c37a2050c11a01dcf9b2c27eba3
Daniel Penelva <d4n.andrade@gmail.com>
2024-09-06 20:26:44 -0300

    feat: add AuthenticationController e o método signupClient
    Este controlador REST AuthenticationController gerencia requisições relacionadas à autenticação e cadastro de clientes em uma aplicação. Ele utiliza a classe de serviço AuthService para executar a lógica de negócios, como verificar a existência de um usuário com um determinado e-mail e cadastrar um novo cliente.
    
    O método signupClient recebe um objeto SignupRequestDTO no corpo da requisição (via @RequestBody). Ele retorna uma ResponseEntity, que encapsula tanto o corpo da resposta quanto o código de status HTTP correspondente. O método verifica se o e-mail informado no SignupRequestDTO já está cadastrado no sistema, chamando o método presentByEmail do AuthService. Se o e-mail já existir, ele retorna uma resposta com o status HttpStatus.NOT_ACCEPTABLE (406) e uma mensagem de erro: "O cliente já existe com este e-mail". Caso o e-mail não esteja em uso, o método chama signupClient no serviço AuthService para cadastrar o novo cliente. O método retorna um objeto UserDto, que contém as informações do cliente recém-cadastrado. Se o cadastro for bem-sucedido, o método retorna uma resposta com o status HttpStatus.OK (200) e o objeto createdUser (os dados do cliente cadastrado).

---
+++ src/main/java/com/api/sistema_anuncio_backend/controller/AuthenticationController.java
@@ -0,0 +1,32 @@
+package com.api.sistema_anuncio_backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.sistema_anuncio_backend.dto.SignupRequestDTO;
+import com.api.sistema_anuncio_backend.dto.UserDto;
+import com.api.sistema_anuncio_backend.services.authentication.AuthService;
+
+@RestController
+public class AuthenticationController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/client/sign-up")
+    public ResponseEntity<?> signupClient(@RequestBody SignupRequestDTO signupRequestDTO) {
+        
+        if (authService.presentByEmail(signupRequestDTO.getEmail())) {
+            return new ResponseEntity<>("O cliente já existe com este e-mail", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        UserDto createdUser = authService.signupClient(signupRequestDTO);
+        return new ResponseEntity<>(createdUser, HttpStatus.OK);
+
+    }
+
+}
