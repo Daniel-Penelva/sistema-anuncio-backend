297df7d773d82c69577c2e8a277c221faf6c239d
Daniel Penelva <d4n.andrade@gmail.com>
2024-09-09 22:52:32 -0300

    feat: add SimpleCorsFilter (CORS)
    Essa classe implementa a interface Filter e é usada para lidar com a política de CORS (Cross-Origin Resource Sharing) em uma aplicação web.
    É anotada com @Component, o que a torna um componente gerenciado pelo Spring Framework, sendo automaticamente registrada no contexto da aplicação.
    É anotada com @Order(Ordered.HIGHEST_PRECEDENCE) que define a prioridade desse filtro, garantindo que ele seja executado antes de outros filtros, caso haja uma cadeia de filtros.
    
    Método doFilter é responsável por interceptar as requisições HTTP e adicionar os cabeçalhos necessários para permitir o compartilhamento de recursos entre origens diferentes (CORS). É constituido por parametros de requisição e resposta HTTP e FilterChain (chain) que define a cadeia de filtros que permite continuar com o processamento da requisição após o filtro ser aplicado.
    Em relação ao processo de CORS, o originHeader é obtido a partir do cabeçalho da requisição (request.getHeader("origin")), que contém o domínio de onde a requisição está sendo feita. Em seguida tem as seguintes configurações de cabeçalhos (1) Access-Control-Allow-Origin: Define quais origens têm permissão para acessar os recursos, (2) Access-Control-Allow-Methods que especifica os métodos HTTP permitidos, (3) Access-Control-Max-Age que define o tempo em segundos pelo qual a resposta de pré-verificação pode ser armazenada em cache e (4) Access-Control-Allow-Headers que permite todos os cabeçalhos com o valor *.
    Para requisição OPTIONS e o método da requisição for OPTIONS, a resposta retorna o status 200 OK imediatamente, sem executar o restante da cadeia de filtros. Para outras requisições (como GET, POST, etc.), o filtro permite que a cadeia continue (chain.doFilter(req, res)).
    Os métodos init que inicializa o filtro, mas aqui não faz nada específico e destroy que é chamado quando o filtro é destruído, mas neste caso também não realiza nenhuma ação.

---
+++ src/main/java/com/api/sistema_anuncio_backend/configs/SimpleCorsFilter.java
@@ -0,0 +1,63 @@
+package com.api.sistema_anuncio_backend.configs;
+
+import java.io.IOException;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class SimpleCorsFilter implements Filter {
+
+    // Construtor SimpleCorsFilter
+    public SimpleCorsFilter(){
+    }
+
+    /**
+     * Método Filter para adicionar todos os cabeçalhos necessários à resposta.
+     *
+     *
+     * @param req servlet request.
+     * @param res servlet response.
+     * @param chain cadeia de filtros para adicionar este novo filtro.
+     * @throws IOException on filtering.
+     * @throws ServletException on filtering.
+     */
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+        
+        String originHeader = request.getHeader("origin");
+        
+        response.setHeader("Access-Control-Allow-Origin", originHeader);
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
