db5a2ebc3f915de15f19c28295ccb56987aa3d28
Daniel Penelva <d4n.andrade@gmail.com>
2024-09-23 20:42:02 -0300

    feat: add o método postAd() no CompanyService e CompanyServiceImpl
    Este método permite a postagem de anúncios (Ads) por um usuário especifico, dando  seu userId e os dados do anúncio fornecidos no AdDTO. Ele envolve interações com o repositório de usuários (UserRepository) e o repositório de anúncios (AdRepository).
    No fluxo do método:
    (1) A função começa buscando o usuário no banco de dados com base no userId fornecido, usando userRepository.findById(userId). O Optional<User> retornado ajuda a lidar com a possibilidade de o usuário não existir. (2) Verificação do Usuário: Se o usuário estiver presente (optionalUser.isPresent()), o código procede para criar o anúncio. Se o usuário não existir, o método retorna false, indicando que a operação falhou. (3) Cria o anúncio, onde um novo objeto Ad é criado e os valores do AdDTO são transferidos para ele, como o nome, descrição, imagem, preço do anúncio e o usuário (user), onde o usuário que criou o anúncio é associado ao anúncio. (4) salva o anúncio, onde o repositório de anúncios (adRepository.save(ad)) é chamado para persistir o novo anúncio no banco de dados. (5) por fim o método retorna true se o anúncio foi criado e salvo com sucesso. Caso contrário, retorna false (se o usuário não foi encontrado, por exemplo).
    O tratamento de exceção IOException, o método declara que pode lançar uma exceção IOException. Isso é relevante porque a conversão de MultipartFile para bytes (usando getBytes()) pode lançar essa exceção se algo der errado no processo de leitura do arquivo.

--- src/main/java/com/api/sistema_anuncio_backend/services/company/CompanyService.java
+++ src/main/java/com/api/sistema_anuncio_backend/services/company/CompanyService.java
@@ -1,5 +1,11 @@
 package com.api.sistema_anuncio_backend.services.company;
 
+import java.io.IOException;
+
+import com.api.sistema_anuncio_backend.dto.AdDTO;
+
 public interface CompanyService {
+
+    boolean postAd(Long userId, AdDTO adDTO) throws IOException;
     
 }
--- src/main/java/com/api/sistema_anuncio_backend/services/company/CompanyServiceImpl.java
+++ src/main/java/com/api/sistema_anuncio_backend/services/company/CompanyServiceImpl.java
@@ -1,18 +1,43 @@
 package com.api.sistema_anuncio_backend.services.company;
 
+import java.io.IOException;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.api.sistema_anuncio_backend.dto.AdDTO;
+import com.api.sistema_anuncio_backend.entity.Ad;
+import com.api.sistema_anuncio_backend.entity.User;
 import com.api.sistema_anuncio_backend.repository.AdRepository;
 import com.api.sistema_anuncio_backend.repository.UserRepository;
 
 @Service
-public class CompanyServiceImpl implements CompanyService{
+public class CompanyServiceImpl implements CompanyService {
 
     @Autowired
     private UserRepository userRepository;
 
     @Autowired
     private AdRepository adRepository;
-    
+
+    // Método que permite a postagem de anúncios (Ads) por um usuário
+    public boolean postAd(Long userId, AdDTO adDTO) throws IOException {
+
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isPresent()) {
+            Ad ad = new Ad();
+            ad.setServiceName(adDTO.getServiceName());
+            ad.setDescription(adDTO.getDescription());
+            ad.setImg(adDTO.getImg().getBytes());
+            ad.setPrice(adDTO.getPrice());
+            ad.setUser(optionalUser.get());
+
+            adRepository.save(ad);
+            return true;
+        }
+
+        return false;
+    }
+
 }
