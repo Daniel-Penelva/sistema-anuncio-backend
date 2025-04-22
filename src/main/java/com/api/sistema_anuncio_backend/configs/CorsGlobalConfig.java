e0b3c3fab419afe2547932d02a8157590a72bbd1
Daniel Penelva <d4n.andrade@gmail.com>
2025-04-22 19:55:31 -0300

    feat: (funcionalidade: deploy - parte 5) criando a classe CorsGlobalConfig
    Continua gerando erro no Cors.
    Adicionei essa configuração complementar além da do WebSecurityConfig

---
+++ src/main/java/com/api/sistema_anuncio_backend/configs/CorsGlobalConfig.java
@@ -0,0 +1,18 @@
+package com.api.sistema_anuncio_backend.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsGlobalConfig implements WebMvcConfigurer{
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://sistemadeanuncio.netlify.app")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+}
